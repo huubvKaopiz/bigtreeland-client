2bf866f50c3dc971d7156116082a19c74c05d4ef
QuynhTX <thanxuanquynh@gmail.com>
2021-10-28 08:31:12 +0700

    chang login page view

--- src/pages/Login/loginForm.tsx
+++ src/pages/Login/loginForm.tsx
@@ -6,6 +6,8 @@ import { actionLogin } from "../../store/auth/slice";
 import { get } from "lodash";
 import { Redirect } from "react-router-dom";
 import useIsMounted from "../../hooks/useIsMounted";
+import validateMessage from "../../lib/validateMessage";
+import logo from "../../assets/image/mainlogo.png";
 
 const { Content } = Layout;
 interface LoginForm {
@@ -33,72 +35,109 @@ function LoginForm(): JSX.Element {
   };
 
   if (userLogin) return <Redirect to="/" />;
-
   return (
     <Layout>
-      <Content style={{ minHeight: 1500 }}>
-        <div
+      <Content>
+        <section
           style={{
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
-            height: 1000,
-            backgroundColor: "white",
+            height: "100vh",
+            backgroundColor: "rgba(240, 242, 245, 0.1)",
           }}
         >
-          <Form
-            style={{
-              width: 500,
-              paddingTop: 40,
-              paddingLeft: 20,
-              paddingRight: 30,
-              paddingBottom: 20,
-            }}
-            name="basic"
-            labelCol={{ span: 6 }}
-            wrapperCol={{ span: 18 }}
-            initialValues={{ remember: true }}
-            onFinish={onFinish}
-            autoComplete="off"
-          >
-            <Form.Item
-              label="Email/Phone"
-              name="email"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your email or phone number!",
-                },
-              ]}
-            >
-              <Input />
-            </Form.Item>
+          <div>
+            <div>
+              <h1
+                style={{
+                  color: "#141414",
+                  fontSize: 48,
+                  fontWeight: 700,
+                  letterSpacing: -0.3,
+                  margin: "0px 0px 15px",
+                  textAlign: "left",
+                  paddingLeft: 20,
+                  lineHeight: "72px",
+                  fontFamily: "sans-serif",
+                }}
+              >
+                Sign In
+              </h1>
+              <h5
+                style={{
+                  color: "#8c8c8c",
+                  fontSize: 20,
+                  letterSpacing: -0.3,
+                  margin: "0px 0px 15px",
+                  textAlign: "left",
+                  paddingLeft: 20,
+                  lineHeight: "30px",
+                  fontFamily: "sans-serif",
+                }}
+              >
+                Enter your email and password to sign in
+              </h5>
+            </div>
 
-            <Form.Item
-              label="Password"
-              name="password"
-              rules={[
-                { required: true, message: "Please input your password!" },
-              ]}
+            <Form
+              style={{
+                display: "block",
+                margin: "auto auto",
+                width: 500,
+                paddingTop: 20,
+                paddingLeft: 20,
+                paddingRight: 30,
+                paddingBottom: 20,
+              }}
+              name="basic"
+              labelCol={{ span: 6 }}
+              wrapperCol={{ span: 18 }}
+              initialValues={{ remember: true }}
+              onFinish={onFinish}
+              layout="vertical"
             >
-              <Input.Password />
-            </Form.Item>
+              <Form.Item
+                label="Email/Phone"
+                name="email"
+                rules={[{ required: true, message: validateMessage.REQUIRE }]}
+              >
+                <Input />
+              </Form.Item>
 
-            <Form.Item
-              name="remember"
-              valuePropName="checked"
-              wrapperCol={{ offset: 6, span: 16 }}
-            >
-              <Checkbox>Remember me</Checkbox>
-            </Form.Item>
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[{ required: true, message: validateMessage.REQUIRE }]}
+              >
+                <Input.Password />
+              </Form.Item>
 
-            <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
-              <Button type="primary" htmlType="submit" loading={loading}>
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
-        </div>
+              <Form.Item
+                name="remember"
+                valuePropName="checked"
+                wrapperCol={{ offset: 0, span: 16 }}
+              >
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+
+              <Form.Item>
+                <Button
+                  style={{ display: "block", width: "100%" }}
+                  type="primary"
+                  htmlType="submit"
+                >
+                  Login
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+          <img
+            style={{ maxWidth: "50%", margin: "auto 0", paddingRight: 200 }}
+            src={logo}
+            alt=""
+          />
+        </section>
       </Content>
     </Layout>
   );
