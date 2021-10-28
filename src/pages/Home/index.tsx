0bf7c7e3a1ec4de775fe8a4fc3ee372146da5e0e
huubv <buivanhuu2017@gmail.com>
2021-10-28 08:29:20 +0700

    demo style component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store/store";
-import {  Layout } from "antd";
+import { Layout } from "antd";
+import Wrapper from "./index.style";
 
 function Home(): JSX.Element {
   const counter = useSelector((state: RootState) => state.counter);
@@ -9,8 +10,10 @@ function Home(): JSX.Element {
   console.log(counter);
 
   return (
-    <Layout.Content style={{minHeight:1500}}>
-      Home content
+    <Layout.Content style={{ minHeight: 1500 }}>
+      <Wrapper>
+        <h1> Home content</h1>
+      </Wrapper>
     </Layout.Content>
   );
 }
