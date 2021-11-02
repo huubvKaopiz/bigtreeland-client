b4cbeb8151ccbf5c696915415f65583625ba35e5
huubv <buivanhuu2017@gmail.com>
2021-11-03 06:39:12 +0700

    implement socket

---
+++ src/global.d.ts
@@ -0,0 +1,5 @@
+export declare global {
+	interface Window {
+		socket: any;
+	}
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store/store";
 import { Layout } from "antd";
 import Wrapper from "./index.style";
+import io from "socket.io-client";
+
+if (process.env.REACT_APP_SOCKET) {
+	window.socket = io(process.env.REACT_APP_SOCKET);
+}
 
 function Home(): JSX.Element {
 	// const counter = useSelector((state: RootState) => state.counter);
 
 	// console.log(counter);
+
+	useEffect(() => {
+		const socket = window.socket;
+		if (!socket) return;
+		socket.on("message", (res: string) => console.log(res));
+
+		socket.on("connect", () => {
+			console.log("socket connected");
+		});
+
+		socket.on("disconnect", () => {
+			console.log("socket disconnect");
+		});
+	}, []);
 
 	return <Layout.Content style={{ height: "100vh" }}>Home content</Layout.Content>;
 }
