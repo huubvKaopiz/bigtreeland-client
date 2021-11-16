4b0f2059cddd7fd069ade6a4dc921b7a95c52b5d
huubv <buivanhuu2017@gmail.com>
2021-11-16 23:03:33 +0700

    update request

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -4,10 +4,9 @@ const apiUrl = process.env.REACT_APP_API;
 
 export const api = axios.create();
 
-api.defaults.baseURL = apiUrl;
-api.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-api.defaults.headers.common["Content-Type"] = "application/json";
-
 export default function request(options: AxiosRequestConfig): Promise<AxiosResponse> {
+	api.defaults.baseURL = apiUrl;
+	api.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+	api.defaults.headers.common["Content-Type"] = "application/json";
 	return api(options);
 }
