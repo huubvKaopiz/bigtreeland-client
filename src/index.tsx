6f6aa05bc343503c45c87d04b0b5cc14f1fd8057
huubv <buivanhuu2017@gmail.com>
2021-11-16 23:20:10 +0700

    update set token for request

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
+import { Provider, useSelector } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import { initLib } from "utils/initLib";
+import { api } from "utils/request";
 import "./index.css";
 import AuthProvider from "./layoutes/AuthProvider";
 import { LoginForm } from "./pages/Login";
 import reportWebVitals from "./reportWebVitals";
-import { persistor, store } from "./store/store";
+import { persistor, RootState, store } from "./store/store";
+import { get } from "lodash";
+import { actionLogout } from "store/auth/slice";
 
 initLib();
+
+function Root() {
+	const access_token = useSelector((state: RootState) => state.auth.user?.access_token ?? "");
+	if (access_token) {
+		api.defaults.headers.common["Authorization"] = "Bearer ".concat(access_token);
+	}
+
+	api.interceptors.response.use(
+		(response) => response,
+		(error) => {
+			if (get(error, "response.status", 1) === 401) {
+				store.dispatch(actionLogout());
+			}
+			throw error;
+		}
+	);
+
+	return (
+		<Router>
+			<Switch>
+				<Route path="/login" component={LoginForm} />
+				<AuthProvider />
+			</Switch>
+		</Router>
+	);
+}
 
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<PersistGate loading={null} persistor={persistor}>
-				<Router>
-					<Switch>
-						<Route path="/login" component={LoginForm} />
-						<AuthProvider />
-					</Switch>
-				</Router>
+				<Root />
 			</PersistGate>
 		</Provider>
 	</React.StrictMode>,
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,8 @@
 import { combineReducers, configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-import { get } from "lodash";
 import { useDispatch } from "react-redux";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import { api } from "utils/request";
-import auth, { actionLogout } from "./auth/slice";
+import auth from "./auth/slice";
 import classReducer from "./classes/slice";
 import employeeReducer from "./employees/slice";
 import parentReducer from "./parents/slice";
@@ -51,20 +49,3 @@ export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export const useAppDispatch: () => AppDispatch = () => useDispatch<AppDispatch>();
-
-store.subscribe(() => {
-	const access_token = get(store.getState(), "auth.user.access_token", "");
-	if (access_token) {
-		api.defaults.headers.common["Authorization"] = "Bearer ".concat(access_token);
-	}
-});
-
-api.interceptors.response.use(
-	(response) => response,
-	(error) => {
-		if (get(error, "response.status", 1) === 401) {
-			store.dispatch(actionLogout());
-		}
-		throw error;
-	}
-);
