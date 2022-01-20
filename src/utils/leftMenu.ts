53a6722eb08d11f68d9d08d616774a61dacdbb00
thanxuanquynh <thanxuanquynh@gmail.com>
2022-01-20 22:17:44 +0700

    update id of sale

--- src/utils/leftMenu.ts
+++ src/utils/leftMenu.ts
@@ -2,5 +2,5 @@ export const leftMenu = [
 	{ id: 1, name: "admin", menuItem: [...Array(100)].map((_, index) => index) },
 	{  id: 2, name: "teacher", menuItem: [1, 3, 5, 6, 13, 15] },
 	{  id: 3, name: "teacher2", menuItem: [1, 3, 5, 6, 13, 15] },
-	{  id: 5, name: "sale", menuItem: [1, 3, 8, 9, 13] },
+	{  id: 4, name: "sale", menuItem: [1, 3, 8, 9, 13] },
 ];
