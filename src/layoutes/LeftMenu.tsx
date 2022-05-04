d3d6edc1393bf8e6b290a4f9df4fd5a51a273176
thanxuanquynh <thanxuanquynh@gmail.com>
2022-05-04 22:20:21 +0700

    fix study-summary class only return 20 class

--- src/layoutes/LeftMenu.tsx
+++ src/layoutes/LeftMenu.tsx
@@ -8,155 +8,169 @@ import {
 	FileOutlined,
 	SettingOutlined,
 	SnippetsOutlined,
-	GiftOutlined
-} from "@ant-design/icons";
-import { Menu } from "antd";
-import { RoleType } from "interface";
-import { get, isArray, isString } from "lodash";
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { useHistory, useLocation } from "react-router-dom";
-import { RootState } from "store/store";
-import { leftMenu, MenuList } from "utils/leftMenu";
+	GiftOutlined,
+} from "@ant-design/icons"
+import { Menu } from "antd"
+import { RoleType } from "interface"
+import { get, isArray, isString } from "lodash"
+import { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
+import { useHistory, useLocation } from "react-router-dom"
+import { RootState } from "store/store"
+import { leftMenu, MenuList } from "utils/leftMenu"
 
-const { SubMenu } = Menu;
+const { SubMenu } = Menu
 
 function LeftMenu(): JSX.Element {
-	const history = useHistory();
-	const userStore = useSelector((state: RootState) => state.auth.user);
-	const [selectedKeys, setSelectedKeys] = useState([""]);
-	const [openKeys, setOpenKeys] = useState([""]);
-	const location = useLocation();
-	const pathname = location.pathname;
-	const [menuItemGranted, setMenuItemGranted] = useState<number[]>([]);
+	const history = useHistory()
+	const userStore = useSelector((state: RootState) => state.auth.user)
+	const [selectedKeys, setSelectedKeys] = useState([""])
+	const [openKeys, setOpenKeys] = useState([""])
+	const location = useLocation()
+	const pathname = location.pathname
+	const [menuItemGranted, setMenuItemGranted] = useState<number[]>([])
 
 	useEffect(() => {
-
+		console.log(pathname)
 		//user
-		if (pathname.includes("/users") || pathname.includes("/user-set-permissions/")) {
-			setOpenKeys([""]);
-			setSelectedKeys(["2"]);
-			return;
+		if (
+			pathname.includes("/users") ||
+			pathname.includes("/user-set-permissions/")
+		) {
+			setOpenKeys([""])
+			setSelectedKeys(["2"])
+			return
 		}
 		//employees
 		if (pathname.includes("/employees")) {
-			setOpenKeys(["employees"]);
-			setSelectedKeys(["3"]);
-			return;
+			setOpenKeys(["employees"])
+			setSelectedKeys(["3"])
+			return
 		}
-		if (pathname.includes("/salaries") || pathname.includes("/salaries-create")) {
-			setOpenKeys(["employees"]);
-			setSelectedKeys(["4"]);
-			return;
+		if (
+			pathname.includes("/salaries") ||
+			pathname.includes("/salaries-create")
+		) {
+			setOpenKeys(["employees"])
+			setSelectedKeys(["4"])
+			return
 		}
 		// study
-		if (pathname.includes("/classes") || pathname.includes("/study")) {
-			setOpenKeys(["classes"]);
-			setSelectedKeys(["5"]);
-			return;
+		if (
+			pathname.includes("/classes") ||
+			(pathname.includes("/study") && !pathname.includes("/study-summary"))
+		) {
+			setOpenKeys(["classes"])
+			setSelectedKeys(["5"])
+			return
 		}
-		if (pathname.includes("/students") || pathname.includes("/students-study-profile")) {
-			setOpenKeys(["classes"]);
-			setSelectedKeys(["6"]);
-			return;
+		if (
+			pathname.includes("/students") ||
+			pathname.includes("/students-study-profile")
+		) {
+			setOpenKeys(["classes"])
+			setSelectedKeys(["6"])
+			return
 		}
 		if (pathname.includes("/parents")) {
-			setOpenKeys(["classes"]);
-			setSelectedKeys(["7"]);
-			return;
+			setOpenKeys(["classes"])
+			setSelectedKeys(["7"])
+			return
 		}
-		if (pathname.includes("/study-summary") || pathname.includes("/study-summary-detail")) {
-			setOpenKeys(["classes"]);
-			setSelectedKeys(["8"]);
-			return;
+		if (
+			pathname.includes("/study-summary") ||
+			pathname.includes("/study-summary-detail")
+		) {
+			setOpenKeys(["classes"])
+			setSelectedKeys(["8"])
+			return
 		}
 		// payment slip
-		
+
 		if (pathname.includes("/payments/payment-slips")) {
-			setOpenKeys(["payments"]);
-			setSelectedKeys(["9"]);
-			return;
+			setOpenKeys(["payments"])
+			setSelectedKeys(["9"])
+			return
 		}
 		if (pathname.includes("/payments/revenue")) {
-			setOpenKeys(["payments"]);
-			setSelectedKeys(["10"]);
-			return;
+			setOpenKeys(["payments"])
+			setSelectedKeys(["10"])
+			return
 		}
 		if (pathname.includes("payments/tuition")) {
-			setOpenKeys(["payments"]);
-			setSelectedKeys(["11"]);
-			return;
+			setOpenKeys(["payments"])
+			setSelectedKeys(["11"])
+			return
 		}
 		if (pathname.includes("/payments")) {
-			setOpenKeys(["payments"]);
-			return;
+			setOpenKeys(["payments"])
+			return
 		}
 		//news
 		if (pathname.includes("/news")) {
-			setSelectedKeys(["12"]);
-			return;
+			setSelectedKeys(["12"])
+			return
 		}
 
 		//gift
 		if (pathname.includes("/gifts")) {
-			setSelectedKeys(["13"]);
-			return;
+			setSelectedKeys(["13"])
+			return
 		}
 		//roles
 		if (pathname.includes("/roles")) {
-			setOpenKeys(["permissions"]);
-			setSelectedKeys(["14"]);
-			return;
+			setOpenKeys(["permissions"])
+			setSelectedKeys(["14"])
+			return
 		}
 		//files
 		if (pathname.includes("/files")) {
-			setSelectedKeys(["15"]);
-			return;
+			setSelectedKeys(["15"])
+			return
 		}
 		//settings
 		if (pathname.includes("/settings")) {
-			setSelectedKeys(["16"]);
-			return;
+			setSelectedKeys(["16"])
+			return
 		}
 
 		// home
 		if (pathname.includes("/")) {
-			setOpenKeys([""]);
-			setSelectedKeys(["1"]);
-			return;
+			setOpenKeys([""])
+			setSelectedKeys(["1"])
+			return
 		}
-
-	}, [pathname]);
+	}, [pathname])
 	// console.log(pathname, selectedKeys)
 
 	useEffect(() => {
-		const menuItem: number[] = [];
+		const menuItem: number[] = []
 		get(userStore, "roles", []).map(({ id, menues }: RoleType) => {
 			// admin === 1
 			console.log(menues)
 			if (id === 1) {
-				menuItem.push(...MenuList.map((menu) => menu.value));
+				menuItem.push(...MenuList.map((menu) => menu.value))
 			} else {
 				if (isString(menues) && menues.length > 2) {
 					const menuList = `${menues}`
 						.substring(1, `${menues}`?.length - 1)
 						.split(",")
-						.map((menu) => +menu);
-					menuItem.push(...menuList);
+						.map((menu) => +menu)
+					menuItem.push(...menuList)
 				} else if (isArray(menues)) {
-					if (menues) menuItem.push(...menues);
+					if (menues) menuItem.push(...menues)
 				}
 			}
-		});
-		setMenuItemGranted([...new Set(menuItem)]);
-	}, [userStore]);
+		})
+		setMenuItemGranted([...new Set(menuItem)])
+	}, [userStore])
 
 	return (
 		<Menu
 			mode="inline"
 			selectedKeys={selectedKeys}
 			openKeys={openKeys}
-			style={{borderRight:'none'}}
+			style={{ borderRight: "none" }}
 		>
 			{menuItemGranted.includes(1) && (
 				<Menu.Item
@@ -199,69 +213,69 @@ function LeftMenu(): JSX.Element {
 				menuItemGranted.includes(6) ||
 				menuItemGranted.includes(7) ||
 				menuItemGranted.includes(8)) && (
-					<SubMenu
-						onTitleClick={() => setOpenKeys(["classes"])}
-						key="classes"
-						icon={<ReadOutlined />}
-						title="QL học tập"
-					>
-						{menuItemGranted.includes(5) && (
-							<Menu.Item key="5" onClick={() => history.push("/classes")}>
-								Lớp học
-							</Menu.Item>
-						)}
-						{menuItemGranted.includes(6) && (
-							<Menu.Item key="6" onClick={() => history.push("/students")}>
-								Học sinh
-							</Menu.Item>
-						)}
-						{menuItemGranted.includes(7) && (
-							<Menu.Item key="7" onClick={() => history.push("/parents")}>
-								Phụ huynh
-							</Menu.Item>
-						)}
-						{menuItemGranted.includes(8) && (
-							<Menu.Item key="8" onClick={() => history.push("/study-summary")}>
-								Bảng tổng kết
-							</Menu.Item>
-						)}
-					</SubMenu>
-				)}
+				<SubMenu
+					onTitleClick={() => setOpenKeys(["classes"])}
+					key="classes"
+					icon={<ReadOutlined />}
+					title="QL học tập"
+				>
+					{menuItemGranted.includes(5) && (
+						<Menu.Item key="5" onClick={() => history.push("/classes")}>
+							Lớp học
+						</Menu.Item>
+					)}
+					{menuItemGranted.includes(6) && (
+						<Menu.Item key="6" onClick={() => history.push("/students")}>
+							Học sinh
+						</Menu.Item>
+					)}
+					{menuItemGranted.includes(7) && (
+						<Menu.Item key="7" onClick={() => history.push("/parents")}>
+							Phụ huynh
+						</Menu.Item>
+					)}
+					{menuItemGranted.includes(8) && (
+						<Menu.Item key="8" onClick={() => history.push("/study-summary")}>
+							Bảng tổng kết
+						</Menu.Item>
+					)}
+				</SubMenu>
+			)}
 			{(menuItemGranted.includes(9) ||
 				menuItemGranted.includes(10) ||
 				menuItemGranted.includes(11)) && (
-					<SubMenu
-						onTitleClick={() => setOpenKeys(["payments"])}
-						key="payments"
-						icon={<DollarOutlined />}
-						title="Thu Chi"
-					>
-						{menuItemGranted.includes(9) && (
-							<Menu.Item
-								key="9"
-								onClick={() => history.push("/payments/payment-slips")}
-							>
-								QL Chi tiêu
-							</Menu.Item>
-						)}
-						{menuItemGranted.includes(10) && (
-							<Menu.Item
-								key="10"
-								onClick={() => history.push("/payments/revenue")}
-							>
-								QL doanh thu
-							</Menu.Item>
-						)}
-						{menuItemGranted.includes(11) && (
-							<Menu.Item
-								key="11"
-								onClick={() => history.push("/payments/tuition")}
-							>
-								QL học phí
-							</Menu.Item>
-						)}
-					</SubMenu>
-				)}
+				<SubMenu
+					onTitleClick={() => setOpenKeys(["payments"])}
+					key="payments"
+					icon={<DollarOutlined />}
+					title="Thu Chi"
+				>
+					{menuItemGranted.includes(9) && (
+						<Menu.Item
+							key="9"
+							onClick={() => history.push("/payments/payment-slips")}
+						>
+							QL Chi tiêu
+						</Menu.Item>
+					)}
+					{menuItemGranted.includes(10) && (
+						<Menu.Item
+							key="10"
+							onClick={() => history.push("/payments/revenue")}
+						>
+							QL doanh thu
+						</Menu.Item>
+					)}
+					{menuItemGranted.includes(11) && (
+						<Menu.Item
+							key="11"
+							onClick={() => history.push("/payments/tuition")}
+						>
+							QL học phí
+						</Menu.Item>
+					)}
+				</SubMenu>
+			)}
 			{menuItemGranted.includes(12) && (
 				<Menu.Item
 					key="12"
@@ -307,9 +321,8 @@ function LeftMenu(): JSX.Element {
 					Hệ thống
 				</Menu.Item>
 			)}
-
 		</Menu>
-	);
+	)
 }
 
-export default LeftMenu;
+export default LeftMenu
--- src/pages/Study/Summary/index.tsx
+++ src/pages/Study/Summary/index.tsx
@@ -1,120 +1,176 @@
-import { CheckCircleOutlined } from '@ant-design/icons';
-import { Button, DatePicker, Layout, List, Modal, Select, Space } from 'antd';
-import useIsAdmin from 'hooks/useIsAdmin';
-import usePermissionList from 'hooks/usePermissionList';
-import { StudySummaryType } from 'interface';
-import { get } from 'lodash';
-import moment, { Moment } from 'moment';
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { actionGetClasses } from 'store/classes/slice';
-import { RootState, useAppDispatch } from 'store/store';
-import { actionDeleteStudySummary, actionGetStudySummaryList } from 'store/study-summary/slice';
-import { isHavePermission } from 'utils/ultil';
-import { CreateStudySummary } from './createModal';
+import { CheckCircleOutlined } from "@ant-design/icons"
+import { Button, DatePicker, Layout, List, Modal, Select, Space } from "antd"
+import useIsAdmin from "hooks/useIsAdmin"
+import usePermissionList from "hooks/usePermissionList"
+import { StudySummaryType } from "interface"
+import { get } from "lodash"
+import moment, { Moment } from "moment"
+import React, { useEffect } from "react"
+import { useSelector } from "react-redux"
+import { useHistory } from "react-router-dom"
+import { actionGetClasses } from "store/classes/slice"
+import { RootState, useAppDispatch } from "store/store"
+import {
+	actionDeleteStudySummary,
+	actionGetStudySummaryList,
+} from "store/study-summary/slice"
+import { isHavePermission } from "utils/ultil"
+import { CreateStudySummary } from "./createModal"
 
-const { confirm } = Modal;
-const { Option } = Select;
+const { confirm } = Modal
+const { Option } = Select
 export function StudySumaryBoard(props: { class_id?: number }): JSX.Element {
-    const { class_id } = props;
-    const history = useHistory();
-    const dispatch = useAppDispatch();
-    const permissionList = usePermissionList();
-	const isAdmin = useIsAdmin();
+	const { class_id } = props
+	const history = useHistory()
+	const dispatch = useAppDispatch()
+	const permissionList = usePermissionList()
+	const isAdmin = useIsAdmin()
 
-    const classList = useSelector((state: RootState) => state.classReducer.classes)
-    const studySummaryList = useSelector((state: RootState) => state.studySummaryReducer.studySummaryList)
-    const getStudySummaryListStatus = useSelector((state: RootState) => state.studySummaryReducer.getStudySummaryListState)
+	const classList = useSelector(
+		(state: RootState) => state.classReducer.classes
+	)
+	const studySummaryList = useSelector(
+		(state: RootState) => state.studySummaryReducer.studySummaryList
+	)
+	const getStudySummaryListStatus = useSelector(
+		(state: RootState) => state.studySummaryReducer.getStudySummaryListState
+	)
 
+	useEffect(() => {
+		if (class_id) {
+			dispatch(actionGetStudySummaryList({ class_id }))
+		} else {
+			dispatch(actionGetStudySummaryList({}))
+			dispatch(actionGetClasses({ per_page: 100000 }))
+		}
+	}, [dispatch, class_id])
 
-    useEffect(() => {
-        if (class_id) {
-            dispatch(actionGetStudySummaryList({ class_id }));
-        } else {
-            dispatch(actionGetStudySummaryList({}));
-            dispatch(actionGetClasses({}));
-        }
-    }, [dispatch, class_id])
+	function handleChangeClass(classID: number) {
+		if (classID > 0) {
+			dispatch(actionGetStudySummaryList({ class_id: classID }))
+		} else {
+			dispatch(actionGetStudySummaryList({}))
+		}
+	}
 
-    function handleChangeClass(classID: number) {
-        if (classID > 0) {
-            dispatch(actionGetStudySummaryList({ class_id: classID }));
-        } else {
-            dispatch(actionGetStudySummaryList({}));
-        }
-    }
+	function onChangeDateFilter(date: Moment | null) {
+		console.log(date)
+	}
 
-    function onChangeDateFilter(date: Moment | null) {
-        console.log(date)
-    }
+	function handleDelete(sID: number) {
+		confirm({
+			title: "Bạn muốn xoá bảng tổng kết này!",
+			icon: <CheckCircleOutlined />,
+			onOk() {
+				dispatch(actionDeleteStudySummary(sID)).finally(() => {
+					if (class_id) {
+						dispatch(actionGetStudySummaryList({ class_id }))
+					} else {
+						dispatch(actionGetStudySummaryList({}))
+					}
+				})
+			},
+			onCancel() {
+				console.log("Cancel")
+			},
+		})
+	}
 
-    function handleDelete(sID: number) {
-        confirm({
-            title: 'Bạn muốn xoá bảng tổng kết này!',
-            icon: <CheckCircleOutlined />,
-            onOk() {
-                dispatch(actionDeleteStudySummary(sID)).finally(() => {
-                    if (class_id) {
-                        dispatch(actionGetStudySummaryList({ class_id }));
-                    } else {
-                        dispatch(actionGetStudySummaryList({}));
-                    }
-                })
-            },
-            onCancel() {
-                console.log('Cancel');
-            },
-        });
-    }
+	return (
+		<Layout.Content>
+			{!class_id && (
+				<Space style={{ marginBottom: 20, marginTop: 20 }}>
+					<DatePicker
+						style={{ width: 200 }}
+						placeholder="Lọc theo quý"
+						onChange={onChangeDateFilter}
+						picker="quarter"
+					/>
+					<Select
+						defaultValue={0}
+						style={{ width: 280 }}
+						onChange={handleChangeClass}
+					>
+						<Option value={0}>Tất cả</Option>
+						{get(classList, "data", []).map((cl) => (
+							<Option value={cl.id} key={cl.id}>
+								{" "}
+								{cl.name}
+							</Option>
+						))}
+					</Select>
+					{(isAdmin ||
+						isHavePermission(permissionList, "study-summary-boards.store")) && (
+						<CreateStudySummary
+							classList={get(classList, "data", [])}
+							class_id={0}
+						/>
+					)}
+				</Space>
+			)}
+			<List
+				itemLayout="horizontal"
+				dataSource={get(studySummaryList, "data", [])}
+				loading={getStudySummaryListStatus === "loading" ? true : false}
+				renderItem={(item: StudySummaryType) => (
+					<List.Item
+						actions={[
+							(isAdmin ||
+								isHavePermission(
+									permissionList,
+									"study-summary-boards.index"
+								)) && (
+								<Button
+									type="link"
+									key="list-loadmore-edit"
+									onClick={() =>
+										history.push({
+											pathname: `/study-summary-detail/${item.id}`,
+											state: { summaryInfo: item },
+										})
+									}
+								>
+									Chi tiết
+								</Button>
+							),
+							(isAdmin ||
+								isHavePermission(
+									permissionList,
+									"study-summary-boards.destroy"
+								)) && (
+								<Button
+									danger
+									type="link"
+									key="list-loadmore-edit"
+									onClick={() => handleDelete(item.id)}
+								>
+									Xoá
+								</Button>
+							),
+						]}
+					>
+						<List.Item.Meta
+							// avatar={<TableOutlined />}
 
-    return (
-        <Layout.Content>
-            {
-                !class_id &&
-                <Space style={{ marginBottom: 20, marginTop: 20 }}>
-                    <DatePicker style={{ width: 200 }} placeholder="Lọc theo quý" onChange={onChangeDateFilter} picker="quarter" />
-                    <Select defaultValue={0} style={{ width: 280 }} onChange={handleChangeClass}>
-                        <Option value={0}>Tất cả</Option>
-                        {get(classList, "data", []).map((cl) => (
-                            <Option value={cl.id} key={cl.id}>
-                                {" "}
-                                {cl.name}
-                            </Option>
-                        ))}
-                    </Select>
-                    { (isAdmin || isHavePermission(permissionList, "study-summary-boards.store")) && 
-                        <CreateStudySummary classList={get(classList, "data", [])} class_id={0} />
-                    }
-                </Space>
-            }
-            <List
-                itemLayout="horizontal"
-                dataSource={get(studySummaryList, "data", [])}
-                loading={getStudySummaryListStatus === 'loading' ? true : false}
-                renderItem={(item: StudySummaryType) => (
-                    <List.Item
-                        actions={[
-                            (
-                                (isAdmin || isHavePermission(permissionList, "study-summary-boards.index")) && 
-                                <Button type="link" key="list-loadmore-edit" onClick={() => history.push({ pathname: `/study-summary-detail/${item.id}`, state: { summaryInfo: item } })}>Chi tiết</Button>),
-                            (
-                                (isAdmin || isHavePermission(permissionList, "study-summary-boards.destroy")) &&
-                                <Button danger type="link" key="list-loadmore-edit" onClick={() => handleDelete(item.id)}>Xoá</Button>)
-                        ]}
-                    >
-                        <List.Item.Meta
-                            // avatar={<TableOutlined />}
-
-                            title={<a
-                                onClick={() => history.push({ pathname: `/study-summary-detail/${item.id}`, state: { summaryInfo: item } })}>
-                                {`${moment(item.from_date).format("DD/MM/YYYY")} - ${moment(item.to_date).format("DD/MM/YYYY")}`}
-                            </a>}
-                            description={get(item, "class.name", "")}
-                        />
-                    </List.Item>
-                )}
-            />
-        </Layout.Content>
-    )
+							title={
+								<a
+									onClick={() =>
+										history.push({
+											pathname: `/study-summary-detail/${item.id}`,
+											state: { summaryInfo: item },
+										})
+									}
+								>
+									{`${moment(item.from_date).format("DD/MM/YYYY")} - ${moment(
+										item.to_date
+									).format("DD/MM/YYYY")}`}
+								</a>
+							}
+							description={get(item, "class.name", "")}
+						/>
+					</List.Item>
+				)}
+			/>
+		</Layout.Content>
+	)
 }
